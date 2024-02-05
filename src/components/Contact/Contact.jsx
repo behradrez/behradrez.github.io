e8721aa316d742be6a8285f9964fc717f145f516
behradrez <r.behrad121@gmail.com>
2024-02-04 22:42:21 -0500

    nitpick

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,14 +8,14 @@ export const Contact = () => {
     <footer className={styles.container} id="contact">
         <div className={styles.textContainer}>
             <h1>Contact Me!</h1>
-            <p>Feel free to reach out on LinkedIn for more information!</p>
+            <p>Reach out on LinkedIn for more information, or check out my projects on Github!</p>
         </div>
         <ul className={styles.links}>
             <li className={styles.link}>
                 <img src={getImageUrl("contact/linkedinIcon.png")}></img>
                 <a href="https://www.linkedin.com/in/behradrezaie/">linkedin.com/behradrezaie</a>
             </li>
-            <li className={styles.link}>
+            <li className={styles.link} href="https://github.com/behradrez">
                 <img src={getImageUrl("contact/githubIcon.png")}></img>
                 <a href="https://github.com/behradrez">github.com/behradrez</a>
             </li>
