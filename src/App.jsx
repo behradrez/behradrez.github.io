388a864841ec1390f32f6e892d4193815e36095d
behradrez <r.behrad121@gmail.com>
2024-03-12 02:20:49 -0400

    Adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from './App.module.css'
-import { Navbar } from './components/navbar/Navbar'
+import { Navbar } from './components/Navbar/Navbar'
 import {Hero} from "./components/Hero/Hero";
 import { About } from './components/About/About';
 import { Experience } from './components/Experience/Experience';
