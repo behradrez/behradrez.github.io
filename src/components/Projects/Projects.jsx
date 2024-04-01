626f58b39a0da186d04492b611bfbaeb1cdb463f
behradrez <r.behrad121@gmail.com>
2024-03-31 22:52:07 -0400

    new project + mapping projects from json

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,29 +2,27 @@ import React from 'react'
 import styles from "./Projects.module.css";
 import { SingleProject } from './individualProject/SingleProject';
 
+import projects from "../../data/projects.json";
+import { getImageUrl } from '../../utils';
 
 export const Projects = () => {
   return (
     <section id="projects" className={styles.container}>
     <h2 className={styles.title}>Projects</h2>
 
+
     <ul className={styles.projectsList}>
-      <li className={styles.specificProject}>
-      <SingleProject name="SportsCenter App" language="Java+Spring" tech1="ReactJS" tech2="PostgreSQL"imageUrl={"projects/sportscenter.png"} description="Full-stack web appp" gitLink="https://github.com/behradrez/" />
-      </li>
-      <li className={styles.specificProject}>
-      <SingleProject name="Hotel Management System" language="Java" tech1="UML" tech2="TDD & BDD"imageUrl={"projects/assetplus.png"} description="Full-stack Java Application" gitLink="https://github.com/behradrez/AssetPlus" />
-      </li>
-      <li className={styles.specificProject}>
-      <SingleProject name="Online Portfolio" language="ReactJS" tech1="Vite" tech2="CSS" imageUrl={"projects/portfolio.png"} description="Portfolio" gitLink="https://github.com/behradrez/behradrez.github.io"  />
-      </li>
-      <li className={styles.specificProject}>
-      <SingleProject name="PyChess" language="Python"  tech2="PyGame" imageUrl={"projects/pychess.png"} description="Chess in Python" gitLink="https://github.com/behradrez/PyChess" />
-      </li>
-      <li className={styles.specificProject}>
-      <SingleProject name="RamQ Detector" language="Python" tech1="OpenPyXL" tech2="OCR" imageUrl={"projects/ramqdetector.png"} description="RamQ detector" gitLink="https://github.com/behradrez/RamQDetector"  />
-      </li>
-    </ul>
+      {projects.map((project) =>  {
+        return (
+          <li className={styles.specificProject}>
+            <SingleProject name={project.name} language={project.language}
+             tech1={project.tech1} tech2={project.tech2} 
+             imageUrl={project.imageUrl} description={project.description}
+             gitLink={project.gitLink}></SingleProject>
+          </li>
+        )
+      })}
+      </ul>
     
     </section>
     
